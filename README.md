59aaac0ba6ba2716a8b94f568dc9c776e43fec40
Alan Franco <alanfr@id.uff.br>
2023-12-19 13:06:35 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # esp-init
-ESP-IDF Project Creator Script
+
+**ESP-IDF Project Creator Script**
+
+This bash script simplifies the process of setting up new projects using the Espressif IoT Development Framework (ESP-IDF) for ESP32 chips. It automates cloning the ESP-IDF repository, creating new projects, and setting up the desired chip architecture.
+
+## Features
+
+- **Clone the ESP-IDF Repository**: Ensures the ESP-IDF is cloned and ready to use.
+- **Create a New Project**: Quickly set up a new project with a specified name and chip architecture.
+- **Set Chip Architecture**: Choose from various supported ESP32 architectures for your project.
+- **Display Help**: Provides usage information and available options.
+
+## Prerequisites
+
+- Git must be installed to clone the ESP-IDF repository.
+- ESP-IDF environment must be properly installed on your system.
+
+## Usage
+
+```bash
+./script_name.sh [OPTION]
+```
+
+### Options
+
+- `--clone`: Clone the ESP-IDF repository.
+- `--new [project_name] [architecture]`: Create a new project. The architecture is optional (default is ESP32).
+- `--help`: Display help information.
+
+### Supported Architectures
+
+- `esp32`
+- `esp32s2`
+- `esp32c3`
+- `esp32s3`
+- `esp32c2`
+- `esp32c6`
+- `esp32h2`
+- `linux`
+- `esp32p4`
+- `esp32c5`
+
+## Installation
+
+1. Clone or download this script to your local machine.
+2. Give execute permission to the script:
+   ```bash
+   chmod +x script_name.sh
+   ```
+3. Run the script with desired options.
+
+## License
+
+Distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. See `LICENSE` for more information.
+
+---
+
+```
+
+```
---
+++ esp-init.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# Initial settings
+WORKDIR=~/esp/esp-projects
+IDF_PATH=~/esp/esp-idf
+
+# Function to clone ESP-IDF
+clone_idf() {
+    if [ ! -d "$IDF_PATH" ]; then
+        echo "Cloning ESP-IDF..."
+        git clone --recursive https://github.com/espressif/esp-idf.git $IDF_PATH
+    else
+        echo "ESP-IDF is already cloned."
+    fi
+}
+
+# Function to update CMakeLists.txt
+update_cmake_lists() {
+    PROJECT_DIR=$1
+    PROJECT_NAME=$2
+
+    # Replace the project line in CMakeLists.txt
+    sed -i "s/project(hello_world)/project(${PROJECT_NAME})/" $PROJECT_DIR/CMakeLists.txt
+    sed -i "s/hello_world_main/main/" $PROJECT_DIR/main/CMakeLists.txt
+}
+
+# Function to set the chip architecture
+set_target() {
+    PROJECT_DIR=$1
+    TARGET=$2
+
+    # Change to the project directory and set the target
+    pushd $PROJECT_DIR
+    idf.py set-target $TARGET
+    popd
+}
+
+# Function to create a new project
+create_project() {
+    PROJECT_NAME=$1
+    TARGET=${2:-esp32}  # Set ESP32 as default if no target is specified
+    PROJECT_DIR=$WORKDIR/$PROJECT_NAME
+
+    if [ -d "$PROJECT_DIR" ]; then
+        echo "A project with this name already exists."
+        exit 1
+    else
+        mkdir -p $PROJECT_DIR
+        cp -r $IDF_PATH/examples/get-started/hello_world/* $PROJECT_DIR
+        echo "Project $PROJECT_NAME created at $PROJECT_DIR."
+
+        # Rename the main file and update CMakeLists.txt
+        mv $PROJECT_DIR/main/hello_world_main.c $PROJECT_DIR/main/main.c
+        update_cmake_lists $PROJECT_DIR $PROJECT_NAME
+
+        # Set the chip architecture
+        set_target $PROJECT_DIR $TARGET
+        
+        # Load the environment
+        source ~/esp/esp-idf/export.sh
+    fi
+}
+
+# Function to display help
+display_help() {
+    echo "Usage: $0 [OPTION]"
+    echo "Options:"
+    echo "  --clone                 Clone the ESP-IDF repository."
+    echo "  --new [project_name] [architecture]" 
+    echo "                                       Create a new project with the"
+    echo "                                      specified name and architecture" 
+    echo "                                      (Default: esp32)."
+    echo "esp32|esp32s2|esp32c3|esp32s3|esp32c2|esp32c6|esp32h2|linux|esp32p4|esp32c5"
+    echo "  --help                  Display this help message."
+}
+
+# Check command line arguments
+if [ $# -eq 0 ]; then
+    display_help
+    exit 1
+fi
+
+case "$1" in
+    --clone)
+        clone_idf
+        ;;
+    --new)
+        if [ $# -lt 2 ]; then
+            echo "Error: Project name not specified."
+            exit 1
+        fi
+        create_project "$2" "$3"
+        ;;
+    --help)
+        display_help
+        ;;
+    *)
+        echo "Invalid option: $1"
+        display_help
+        exit 1
+        ;;
+esac
